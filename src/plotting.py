076706710760dd28524ae0ec927b67944e90b5e5
Adrian Lopez <adrian1820@mail.fresnostate.edu>
2026-06-08 11:27:42 -0700

    Remove plot_vf2 function from plotting.py
    Removed the plot_vf2 function that visualizes training and testing predictions.

--- src/plotting.py
+++ src/plotting.py
@@ -182,67 +182,6 @@ def rename_pfi_features(df):
     return df
 
 
-def plot_vf2(county, train_dates, train_actuals, train_predictions,
-             test_dates, test_actuals, test_predictions):
-
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(
-        x=train_dates,
-        y=train_actuals,
-        mode="markers+lines",
-        name="Train (True)"
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=train_dates,
-        y=train_predictions,
-        mode="markers+lines",
-        name="Train Pred."
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=test_dates,
-        y=test_actuals,
-        mode="markers+lines",
-        name="Test (True)"
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=test_dates,
-        y=test_predictions,
-        mode="markers+lines",
-        name="Test Pred."
-    ))
-
-    fig.update_layout(
-        xaxis_title="Time",
-        yaxis_title="Case Rate",
-        autosize=False,
-        width=1300,
-        height=1080,
-        font=dict(size=34),
-        template="plotly_white",
-        xaxis=dict(gridcolor="rgba(0,0,0,0.15)"),
-        yaxis=dict(gridcolor="rgba(0,0,0,0.15)"),
-        legend=dict(
-            x=1,
-            y=0.85,
-            xanchor="right",
-            yanchor="bottom",
-            bgcolor="white",
-            bordercolor="black",
-            borderwidth=2
-        )
-    )
-
-    fig.update_traces(
-        line=dict(width=7),
-        marker=dict(size=14)
-    )
-
-    return fig
-
 
 def plot_mean_pfi2(df, county_name, bar_color="royalblue"):
     """
